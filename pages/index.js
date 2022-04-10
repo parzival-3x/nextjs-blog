d8d9bf1df2cc0ea3cdc411724bf49abb0337d5a4
parzival-3x <bg193b@gmail.com>
2022-04-09 20:46:39 -0500

    added links to blog posts

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import Date from '../components/date'
 import { getSortedPostsData } from '../lib/posts'
 
 export async function getStaticProps() {
@@ -27,9 +28,7 @@ export default function Home({allPostsData}) {
           <a href="https://www.linkedin.com/in/benjamin-guzman-4b6732235/">here</a>
         </p>
         <p>
-          <Link href="/posts/first-post">
-          <a>Go to second page</a>
-          </Link>
+          Second page unavaliable
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
@@ -37,12 +36,14 @@ export default function Home({allPostsData}) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
+            <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
           ))}
         </ul>
       </section>
